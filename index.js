3dcea4ecce8e7d3780741cb6e7109c6bf66e61bd
Ny Huynh <39287544+NyHuynh@users.noreply.github.com>
2018-10-12 10:42:07 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
 const Joi = require('joi');
+const morgan = require('morgan');
+const helmet = require('helmet');
+
+const logger = require('./logger');
 const express = require('express');
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static('Public'));
+app.use(helmet());
 
+if(app.get('env') === 'development'){
+    app.use(morgan('tiny'));
+    console.log('Morgan enabled...');
+}
+
+app.use(logger);
+
+
+app.use(function(req, res, next) {
+    console.log('Authentication...');
+    next();
+});
 const courses = [
     {id:1, name: 'Course1'},
     {id:2, name: 'Course2'},
