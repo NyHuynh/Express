ce2541bc72b7373a985efa45284524bd59487439
Ny Huynh <huynhthiny.nct@gmail.com>
2018-10-15 11:04:31 +0700

    work with mongo - CRUD

--- mongo-demo/index.js
+++ mongo-demo/index.js
@@ -6,22 +6,91 @@ mongoose.connect('mongodb://localhost/playground')
     .catch(err => console.err('Could not connect to MongoDB', err));
 
 const courseSchema = new mongoose.Schema({
-    name: String,
+    name: {
+        type: String,
+        required: true,
+        minlength: 5,
+        maxlength: 255
+    },
+    category: {
+        type: String,
+        enum: ['web', 'mobile', 'network']
+    },  
     author: String,
-    tag: [String],
+    tag: {
+        type: Array,
+        validate: {
+            validator: function(v) {
+                return v && v.length > 0;
+            },
+            message: 'A course should have at least 1 tag.'
+        }
+    },
     date: {type: Date, default: Date.now},
-    isPublished: Boolean    
+    isPublished: Boolean,
+    price: {
+        type: Number,
+        required: function(){return this.isPublished;},
+        min: 10,
+        max:200
+    }
 });
+
+const Course = mongoose.model('Course', courseSchema);
+
 async function createCourse() {
-    const Course = mongoose.model('Course', courseSchema);
     const course = new Course({
-        name: 'Node.js Course',
+        name: 'Angular Course',
+        category: 'web',
         author: 'Mosh',
-        tag: ['Node', 'backend'],
-        isPublished: true
+        tag: ['Angular', 'frontend'],
+        isPublished: true,
+        price: 16
     });
-    
-    const result = course.save();
-    console.log(result);    
+
+    try {
+        const result = await course.save();
+        console.log(result); 
+    } catch (error) {
+        console.log(error.message);
+    }
+       
 }
-createCourse();
+
+// createCourse();
+
+async function getCourse(){
+    const courses = await Course
+    .find({author: 'Mosh', isPublished: true})
+    .limit(10)
+    .sort({name: 1})
+    .select({name: 1, tag: 1});
+    console.log(courses);
+}
+
+// getCourse();
+
+
+
+async function updateCourse(id){
+    const course = await Course.findByIdAndUpdate(id,{
+        $set: {
+            isPublished: false,
+            author: 'Ny Huynh'
+        }
+    });
+
+    console.log(course);
+}
+
+// updateCourse('5bc3ff7e6171e129d40fd857');
+
+async function removeCourse(id){
+    const course = await Course.findByIdAndRemove(id);
+
+    console.log(course);
+}
+
+// removeCourse('5bc3ff7e6171e129d40fd857');
+
+
